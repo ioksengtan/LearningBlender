576c6b36c8e69819e42ae3a92a0115be59a97247
Yu-Sheng Chen <yushengc@Yu-ShengdeMacBook-Pro.local>
2021-01-02 20:43:24 +0800

    20210102_4

--- db2json.py
+++ db2json.py
@@ -5,8 +5,8 @@ lines = f.readlines()
 rows = []
 for line in lines:
 	reg = {}
-	data = line.split('\t')
-	if len(data) != 6:
+	data = line.split('\n')[0].split('\t')
+	if len(data) != 8:
 		continue
 	reg['source'] = data[0]
 	reg['link'] = data[1]
@@ -14,12 +14,13 @@ for line in lines:
 	reg['description'] = data[3]
 	reg['thumbnail'] = data[4]
 	reg['date'] = data[5]
-    reg['tags'] = data[6]
-    reg['object'] = data[7]
+	reg['tags'] = data[6]
+        reg['object'] = data[7]
 	rows.append(reg)	
 f.close()
 data = {}
 data['table'] = rows
+print('total ' + str(len(rows)) + ' rows imported') 
 f = open('data.json.raw','w')
 json.dump(data, f)
 f.close()
